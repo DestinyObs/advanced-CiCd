8a206c873e1802ae81b4279a40c7eab2643011d1
Ubuntu <ubuntu@ip-172-31-93-106.ec2.internal>
2024-12-18 04:43:22 +0000

    fixing ssh connection

--- output.tf
+++ output.tf
@@ -18,7 +18,18 @@ output "jenkins_master_public_ip" {
   value       = aws_instance.jenkins_master.public_ip
 }
 
+output "jenkins_master_public_dns" {
+  description = "Public DNS of the Jenkins Master instance"
+  value       = aws_instance.jenkins_master.public_dns
+}
+
 output "jenkins_agent_public_ip" {
   description = "Public IP of the Jenkins Agent instance"
   value       = aws_instance.jenkins_agent.public_ip
 }
+
+output "jenkins_agent_public_dns" {
+  description = "Public DNS of the Jenkins Agent instance"
+  value       = aws_instance.jenkins_agent.public_dns
+}
+
